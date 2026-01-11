d6421f20a5d5c9b76d3632ab28542cd883ddf39a
WithFortuna <rmsghchl0@naver.com>
2026-01-11 20:28:38 +0900

    init: Spring Cloud Config 서버

--- config-service/src/test/java/cleanhouse/configservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/cleanhouse/configservice/ConfigServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package cleanhouse.orderservice;
+package cleanhouse.configservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrderServiceApplicationTests {
+class ConfigServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
